1fc41889e9c99d201f8283bc328647a221111fbc
Eric <1eric.hoffman@gmail.com>
2020-02-24 06:52:08 -0600

    attampted to implement tests

---
+++ src/test/java/FacilityMaintenanceTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+// Wasn't able to get to this in time
+public class FacilityMaintenanceTest {
+
+    FacilityMaintenanceTest myTest = new FacilityMaintenanceTest();
+
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void makeFacilityMaintRequest() {
+    }
+
+    @Test
+    public void listFacilityMaintRequest() {
+    }
+}
