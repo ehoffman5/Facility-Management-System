69b8efc6c9d57897d6c74648f7e17f02597b38b5
Eric <1eric.hoffman@gmail.com>
2020-03-15 23:28:53 -0500

    started test code

---
+++ src/test/java/FacilityBaseTest/facilityBaseTest.java
@@ -0,0 +1,26 @@
+/* package facilityBaseTest;
+
+import database.DBConnector;
+import facilityBase.FacilityDetails;
+import facilityBase.FacilityCapacity;
+import java.sql.*;
+import java.util.*;
+
+public class facilityBaseTest;
+{
+public void setFacilityNumber(int facilityNumber);
+        SetFacilityNumber setFacilityNumber = new setFacilityNumber
+        Assert.assertEquals(int, FacilityBase.facilityNumber());
+
+public void setFacilityDetails(FacilityDetails facilityDetail);
+        SetFacilityDetails setFacilityDetails = new FacilityDetails
+        Assert.assertEquals(facilityDetail, FacilityBase.facilityDetail());
+
+public void setFacilityCapacity(int facilityCapacity);
+        SetFacilityCapacity setFacilityCapacity = new setFacilityCapacity
+        Assert.assertEquals(int,FacilityBase.facilityCapacity());
+
+public void setFacilityName(String facilityName);
+        SetFacilityName setFacilityName = new FacilityName
+        Assert.assertEquals(facilityName,FacilityBase.facilityName());
+        } */
