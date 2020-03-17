fb02ba1dab0dbcaabdb312f3a523bccfe2af569c
Eric <1eric.hoffman@gmail.com>
2020-03-17 16:03:10 -0500

    created the beans

--- src/main/java/facilityBase/FacilityCapacity.java
+++ src/main/java/facilityBase/FacilityCapacity.java
@@ -3,18 +3,18 @@ package facilityBase;
 public class FacilityCapacity extends Facility {
 
     // Variable declarations
-    private int facilityCapacity;
+    private int capacity;
 
     // Constructor to initialize class attributes
     public FacilityCapacity() { }
 
 
     // ---------- Getter and Setter Methods ---------- //
-    public void setCapacity(int facilityCapacity) {
-        this.facilityCapacity = facilityCapacity;
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
 
     public int getCapacity() {
-        return facilityCapacity;
+        return capacity;
     }
 }
